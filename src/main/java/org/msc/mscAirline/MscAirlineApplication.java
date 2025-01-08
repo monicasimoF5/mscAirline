8f1a4c24d34438952d8127116ca90c74a49de694
monica <mosice@gmail.com>
2025-01-08 15:11:00 +0100

    first commit

---
+++ src/main/java/org/msc/mscAirline/MscAirlineApplication.java
@@ -0,0 +1,13 @@
+package org.msc.mscAirline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscAirlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscAirlineApplication.class, args);
+	}
+
+}
