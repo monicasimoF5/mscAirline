ad21114e848e8a7df49c5f9b37d78f4b8afc0274
monica <mosice@gmail.com>
2025-01-15 11:17:18 +0100

    feat: create class Flight

---
+++ src/main/java/org/msc/mscAirline/flights/Flight.java
@@ -0,0 +1,94 @@
+package org.msc.mscAirline.flights;
+
+import jakarta.persistence.*;
+import org.msc.mscAirline.airports.Airport;
+
+import java.sql.Date;
+
+@Entity
+public class Flight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "origin_id")
+    Airport origin;
+
+    @OneToOne
+    @JoinColumn(name = "destination_id")
+    private Airport destination;
+
+    private int availableSeats;
+    private Date departureTime;
+    private Date arrivalTime;
+
+    public Flight(Airport origin, Airport destination, int availableSeats, Date departureTime, Date arrivalTime) {
+        this.origin = origin;
+        this.destination = destination;
+        this.availableSeats = availableSeats;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+    }
+
+    public Flight(Long id, Airport origin, Airport destination, int availableSeats, Date departureTime, Date arrivalTime) {
+        this.id = id;
+        this.origin = origin;
+        this.destination = destination;
+        this.availableSeats = availableSeats;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+    }
+
+    public Flight() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Airport getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(Airport origin) {
+        this.origin = origin;
+    }
+
+    public Airport getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Airport destination) {
+        this.destination = destination;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public void setAvailableSeats(int availableSeats) {
+        this.availableSeats = availableSeats;
+    }
+
+    public Date getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(Date departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public Date getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(Date arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+}
