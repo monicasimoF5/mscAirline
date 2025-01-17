e2770710f0111fc4cb098a2a6713b669164805ac
monica <mosice@gmail.com>
2025-01-17 11:51:00 +0100

    config: add bean passwordEncoder in SecurityConfiguration

--- src/main/java/org/msc/mscAirline/config/SecurityConfiguration.java
+++ src/main/java/org/msc/mscAirline/config/SecurityConfiguration.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -61,6 +63,11 @@ public class SecurityConfiguration {
         httpSecurity.headers(header -> header.frameOptions(frame -> frame.sameOrigin()));
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
