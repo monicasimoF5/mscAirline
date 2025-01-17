4c5fcb9e5572c4001d12ab013858e8f08b1a78b0
monica <mosice@gmail.com>
2025-01-17 11:13:17 +0100

    config: create AuthenticationController

---
+++ src/main/java/org/msc/mscAirline/authentication/AuthenticationController.java
@@ -0,0 +1,31 @@
+package org.msc.mscAirline.authentication;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path = "${api-endpoint}")
+public class AuthenticationController {
+
+    @GetMapping(path = "/login")
+    public ResponseEntity<Map<String, String>> login(){
+        SecurityContext contextHolder = SecurityContextHolder.getContext();
+        Authentication authentication = contextHolder.getAuthentication();
+
+        Map<String, String> json = new HashMap<>();
+        json.put("message", "Logged");
+        json.put("username", authentication.getName());
+        json.put("roles", authentication.getAuthorities().iterator().next().toString());
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(json);
+    }
+}
